bbb1953cc955a89556b09273ab7710324ad88fe1
Anna <annadalex@icloud.com>
2024-03-18 21:00:30 +0100

    Terminato

--- laravel-migration-seeder/app/Http/Controllers/Guest/TrainController.php
+++ laravel-migration-seeder/app/Http/Controllers/Guest/TrainController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Trains;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+
+        $trains= Trains::where('Orario_di_partenza','>=',today())->get();
+        return view('pages.welcome',compact('trains'));
+    }
 }
